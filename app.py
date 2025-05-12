67ee605397494211cd5830aa9aaa45884971ead4
yehorkalchenko81 <egorkalcenko69@gmail.com>
2025-05-12 20:45:28 +0300

    test login

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 
 
-st.title("Firt ver")
+st.title('Login version, test')
 
-st.write("It's write")
+config = {'users': {'email': ['1'], 'password': ['1']}}
 
-if st.button("Click"):
-    st.write("You got it")
+authenticator = stauth.Authenticate(
+    config['users']
+)
+
+try:
+    authenticator.login()
+except Exception as e:
+    st.error(e)
+
+if st.session_state.get('authentication_status'):
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state.get("name")}*')
+    st.title('Some content')
+elif st.session_state.get('authentication_status') is False:
+    st.error('Username/password is incorrect')
+elif st.session_state.get('authentication_status') is None:
+    st.warning('Please enter your username and password')

4287ad008beadadce70cfa1704a580d6791ff8e9
yehorkalchenko81 <egorkalcenko69@gmail.com>
2025-05-12 23:33:07 +0300

    Test button

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
+import yaml
+from yaml.loader import SafeLoader
 
-st.title('Login version, test')
-
-config = {
-    'credentials': {
-        'usernames': {
-            'yhkal': {
-                'email': '1', 
-                'name': 'Yehor Kalchenko',
-                'password': '1'
-            }
-        }
-    }
-}
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
-    'cookie_name',         # будь-яка назва
-    'signature_key',       # будь-який секретний ключ
-    cookie_expiry_days=30  # опціонально
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
 )
 
-try:
-    authenticator.login()
-except Exception as e:
-    st.error(e)
+if 'show_main_title' not in st.session_state:
+    st.session_state.show_main_title = True
+
+if st.session_state.show_main_title:
+    st.title('Login version, test')
+
+def login():
+    try:
+        authenticator.login()
+    except Exception as e:
+        st.error(e)
 
 if st.session_state.get('authentication_status'):
-    authenticator.logout()
+    if st.button('Logout'):
+        st.session_state['authentication_status'] = None
+        st.session_state.show_main_title = True
+    st.session_state.show_main_title = False
     st.write(f'Welcome *{st.session_state.get("name")}*')
     st.title('Some content')
 elif st.session_state.get('authentication_status') is False:
     st.error('Username/password is incorrect')
+    login()
 elif st.session_state.get('authentication_status') is None:
     st.warning('Please enter your username and password')
+    login()

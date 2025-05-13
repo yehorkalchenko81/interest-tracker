f9109276dc42c560e334f816f91e8abc1e5b451c
yehorkalchenko81 <egorkalcenko69@gmail.com>
2025-05-13 19:26:47 +0300

    extended html

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
+support_services = ('Instagram', 'Reddit')
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', name='UserName', services=support_services)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', name='UserName')
 
 if __name__ == '__main__':
     app.run(debug=True)
